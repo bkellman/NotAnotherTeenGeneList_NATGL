bc29f46c48f7c3988c64b5ca3761d113cb527aa7
Benjamin P. Kellman <bkellman@ucsd.edu>
2016-02-19 14:34:17 -0800

    first commit: ramigo, biomart, genemania weighted distance gene clustering

---
+++ query.functions.r
@@ -0,0 +1,175 @@
+source('HuNet_Distance/HuNet.func.R')
+library(reshape)
+library(igraph)
+library(RamiGO)
+#library(graph)
+#library(Rgraphviz)
+
+load_HuNet <- function(threshold=.7){
+	# Make a graph (g, class: graphNEL) from Human PPI db (Hippie)
+	g <- Make_Protein_Graph(threshold)  
+
+	# convert g to 'igraph' graph
+	HuNet <- igraph.from.graphNEL(g, name = TRUE, weight = TRUE,unlist.attrs = TRUE)
+	HuNet <- HuNet %>% set_vertex_attr("label", value = V(HuNet)$name)
+	return(HuNet)
+}
+
+load_GeneMania <- function(combined=TRUE,type=c('Physical_Interaction','Predicted','Genetic_Interactions','Pathway','Co-localization','Co-expression','Shared_protein_domains'),alltypes=FALSE){
+	if(combined){
+		r <- read.csv('../data/GeneMania/combined/COMBINED.DEFAULT_NETWORKS.BP_COMBINING.txt',sep='\t')
+		g <- graph.data.frame(r,directed=FALSE)
+	}else{
+		ls <- system('ls ../data/GeneMania/individual/*.txt',intern=TRUE)
+		rL <- list()
+		for(l in ls){
+			lp <- strsplit(l,split='\\.')[[1]]
+			if(lp[1]==type | alltypes){
+				rL[[l]] <- read.csv(paste('../data/GeneMania/individual/',l,sep=''),sep='\t')
+				rL[[l]]$type <- lp[1]
+				rL[[l]]$database <- lp[2]
+			}
+		}
+		r <- do.call(rbind,rL)
+		g <- graph.data.frame(r,directed=FALSE)
+	}
+	g
+}
+
+Network_ShortestPaths_distance <- function(terms,Network,func=function(x) x,weighted=TRUE){
+	sp <- shortest.paths(Network, v=terms, to=terms ,mode = "all",mode=ifelse(weighted,'automatic','unweighted'))
+	func(sp)
+}
+
+GO_jaccard<-function(BM,gene_ids,gene_type,ramigo_search=FALSE,ramigo_paths=FALSE,debug=FALSE){
+	O <- list()
+	for(g_i in gene_ids){
+		if(debug){print(paste('get GO for',g_i))}
+		o_i = BM$go_id[BM[gene_type]==g_i]
+		if(nchar(paste(o_i,collapse=''))>0 ){
+			if(ramigo_search){
+				O[[g_i]] = GO_Ramigo_flatExpand(o_i)
+			}else{
+				O[[g_i]]  = o_i
+			}
+		}
+	}
+	
+	sim <- matrix(0,length(gene_ids),length(gene_ids),dimnames=list(gene_ids,gene_ids))
+	for(g_i in gene_ids){
+		for(g_j in gene_ids){
+			o_i = O[[g_i]]
+			o_j = O[[g_j]]
+			if(nchar(paste(o_i,collapse=''))>0 & nchar(paste(o_j,collapse=''))>0){
+				if(ramigo_paths){
+					sim[g_i,g_j] <- GO_Ramigo_mostRecentAncestor(o_i,o_j)
+				}else{
+					sim[g_i,g_j] <- length(intersect(o_i,o_j)) / length(union(o_i,o_j))
+				}
+			}			
+		}
+	}
+	filter <- apply(sim,1,sum)>0 | apply(sim,2,sum)>0
+	sim[filter,filter]
+	sim
+}
+
+Ramigo_wrapper <- function(go,attempts=10,file='ramigo.dot'){
+	dotRes <- NULL
+	while(is.null(dotRes)){
+		try( dotRes <- getAmigoTree(goIDs=go, picType="dot", saveResult=TRUE,filename=file) )
+		attempts = attempts - 1
+		if(attempts==0){stop('server cannot be reached with multiple attempts')}
+	}
+	#tt <- readAmigoDot(object=dotRes)
+	tt <- readAmigoDot_man(file)
+}
+
+readAmigoDot_man <- function(file){
+	r <- readLines('ramigo.dot')
+	edges <- list()
+	nodes <- list()
+	for(r_i in r){
+		if(grepl('->',r_i)){
+			# edge
+			edges[[r_i]] <- gsub('\t','',strsplit(r_i,' ')[[1]][c(1,3)])
+		}else if(grepl('GO|UBERON|CARO|CL|OBA',r_i )) {
+			# node
+			nodes[[r_i]] <- strsplit(r_i,'>|<')[[1]][8]
+		}else{
+			print(paste('skip',r_i))
+		}
+	}
+	nodes<-unlist(nodes)
+	edges<-lapply(edges,function(x) unlist(nodes[as.numeric(unlist(gsub('node','',x)))]))
+	eL <-do.call(rbind,edges)
+	rownames(eL) <- NULL
+	colnames(eL) <- c('parent','child')
+	graph.data.frame(eL,directed=FALSE)
+}
+
+GO_Ramigo_flatExpand <- function(go){
+	tmp<<-go
+	tt <- Ramigo_wrapper(go)
+	#dotRes <- getAmigoTree(goIDs=go, picType="dot", saveResult=F)
+	#tt <- readAmigoDot(object=dotRes)
+	#tt@annot$GO_ID
+	as.character(V(tt)$name)
+}
+
+
+GO_Ramigo_mostRecentAncestor <- function(go1,go2,func=median){
+	go <- c(go1,go2)
+	tt <- Ramigo_wrapper(go)
+	#dotRes <- getAmigoTree(goIDs=go, picType="dot", saveResult=F)
+	#tt <- readAmigoDot(object=dotRes)
+	#adjM <- tt@adjMatrix
+	#rownames(adjM)=colnames(adjM)=tt@annot$GO_ID
+	#graph <- graph.adjacency(adjM, mode="undirected") # shortest path in an undirected graph will give twice the distance to the "most recent common ancestor"
+	graph <- tt
+	sp <- shortest.paths(graph, v=go1[-(go1 %in% go2)], to=go2[-(go2 %in% go1)] ,mode = "all")
+	func(sp)
+}
+
+combine <- function(dL , func,BM,gene_ids,gene_type,weight=rep(1,length(dL))){
+	#initialize final distance matrix
+	d <- matrix(0,length(gene_ids),length(gene_ids),dimnames=list(gene_ids,gene_ids))
+	
+	# convert to gene_type names
+	for(i in 1:length(dL)){
+		d_i = dL[[i]]
+		d_i_id <- names(which.max(apply(BM,2,function(x) sum(rownames(d_i) %in% x))))
+		if(!all(rownames(d_i)==colnames(d_i))){stop('distance matrix is not symetrical')}
+		rownames(d_i)=colnames(d_i)=convert(rownames(d_i),d_i_id,gene_type,BM)
+		dL[[i]] = d_i
+	}
+	# apply weights
+	for(i in 1:length(dL)){
+		dL[[i]] = dL[[i]] * weight[i]
+	}
+	# combine distance matrixes to d
+	for(i in gene_ids){
+		for(j in gene_ids){
+			d[i,j] = func(na.omit(as.vector(unlist(lapply(dL,function(x) get_dij(x,i,j))))))
+		}
+	}
+	return(d)
+}
+
+get_dij <- function(d,i,j){
+	if(i %in% rownames(d) & j %in% colnames(d)){
+		d[i,j]
+	}else{
+		median(d)
+		print(paste(i,'or',j,'not found in d'))
+	}
+}
+
+convert <- function(a,aname,bname,BM){
+	unlist(sapply(a,function(a_i) unique( BM[[bname]][which(BM[[aname]]==a_i)])))
+}
+
+
+
+
+
---
+++ query.r
@@ -0,0 +1,122 @@
+source('query.functions.r')
+library(biomaRt)
+library(xlsx)
+library(KEGGREST)
+library(gplots)
+library(ggplot2)
+
+override=FALSE
+signed=FALSE
+
+ensembl=useMart("ensembl",'hsapiens_gene_ensembl')
+
+r1 <- read.xlsx('geneList/Courchesne_summary_10062015.xlsx',sheetIndex=1)
+r2 <- read.csv('geneList/tito_list_clinical_data.txt')[,1:5]
+gene_ids <- unique(as.character(r1$accession))
+gene_ids <- unlist(lapply(strsplit(gene_ids,'\\.'),function(x) x[1]))
+r <- merge(r1,r2,by.x='Individual',by.y='subjectid')
+
+gene_type = 'refseq_mrna'
+attributes <- c(gene_type)
+
+# get gene_ids
+attributes <- c(attributes,'hgnc_symbol','ensembl_gene_id','entrezgene')#,'hsa ')
+
+# get phenotype (OMIN)
+#attributes <- c(attributes,'mim_morbid_description') # ,'mim_gene_accession','mim_gene_description')
+
+# get ontology (GO)
+attributes <- c(attributes,'go_id')
+
+# run BM query
+BM = getBM(attributes=attributes, filters=gene_type, values=gene_ids, mart=ensembl)
+gene_ids <- unique(BM$refseq_mrna)
+
+# get KEGG pathwasy
+p = keggLink('hsa','pathway')
+map = split(names(p),unname(p))
+
+# cluster by tissue expression (BioGPS, GNF, GeTX)
+GTEx_file <- '../data/GTEx/GTEx_Analysis_v6_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct'
+if(!file.exists('../data/tmp.txt') | override){
+	system(paste("grep -E '",paste(c('Name',unique(BM$ensembl_gene_id)),collapse="|"),GTEx_file,"' > ../data/tmp.txt",sep=''))
+}
+expression_annot <- read.csv('GTEx/GTEx_Data_V6_Annotations_SampleAttributesDS.txt',sep='\t')
+expression <- read.csv('tmp.txt',sep='\t')
+rownames(expression) <- unlist(lapply(strsplit(as.character(expression$Name),'\\.'),function(x) x[1]))
+expression <- expression[,-c(1:2)]
+expression <- data.matrix(expression) # make data matrix
+expression <- log(expression+1) # approximate gaussian
+expression <- scale(expression) # normalize values
+#tissue_site <- data.frame(do.call(rbind,strsplit(colnames(expression),'\\.')))$X3
+tissue_site_df <- expression_annot[match(gsub('\\.','-',colnames(expression)),as.character(expression_annot$SAMPID)),c('SMTS','SMTSD')]
+tissue_site <- tissue_site_df$SMTSD
+
+#heatmap.2(data.matrix(expression),
+#	ColSideColors=rainbow(length(levels(tissue_site)))[tissue_site],trace='none',
+#	labRow=convert(rownames(expression),'ensembl_gene_id','hgnc_symbol',BM ))
+tissue_site <- tissue_site[order(tissue_site)]
+tissue_site_nam <- ifelse( duplicated(tissue_site) , '' , as.character(tissue_site))
+pdf('clusters_out/tissue_expression.pdf',width=15)
+heatmap.2(data.matrix(expression)[,order(tissue_site)],Colv=FALSE,
+	ColSideColors=c(rainbow(length(levels(tissue_site)))[tissue_site]),trace='none',
+	labRow=convert(rownames(expression),'ensembl_gene_id','hgnc_symbol',BM ),
+	labCol=tissue_site_nam,mar=c(10,5))
+dev.off()
+
+
+######### compute distance
+dL <- list()
+
+# GO distance
+dL[['GO_jaccard']] <- 1 - GO_jaccard(BM,gene_ids,gene_type)
+dL[['GO_jaccard_ramigo_flat']] <- 1 - GO_jaccard(BM,gene_ids,gene_type,ramigo_search=TRUE,ramigo_paths=FALSE)
+dL[['GO_jaccard_ramigo_paths']] <- 1 - GO_jaccard(BM,gene_ids,gene_type,ramigo_search=FALSE,ramigo_paths=TRUE)
+
+
+# Tissue expression
+dL[['abs_tissue_cor_pearson']] <- 1-abs( cor(t(expression[,-c(1:2)]+1)) )
+
+
+# GeneMania combined
+GM_comb <- load_GeneMania(combined=TRUE)
+dL[['geneMania_combined_net_weightedShortestPath']] <- Network_ShortestPaths_distance(unique(BM$ensembl_gene_id),GM_comb,weighted=TRUE,func=normalize)
+dL[['geneMania_combined_net_unweightedShortestPath']] <- Network_ShortestPaths_distance(unique(BM$ensembl_gene_id),GM_comb,weighted=FALSE)
+
+
+types<-c('Physical_Interaction','Predicted','Genetic_Interactions','Pathway','Co-localization','Co-expression','Shared_protein_domains')
+# GeneMania individual
+normalize <- function(x) x/max(x)
+for(t_i in types){
+	GM_df <- load_GeneMania(combined=FALSE,type=t_i)
+	dL[[paste('geneMania_',t_i,'_net_weightedShortestPath',sep='')]] <- Network_ShortestPaths_distance(unique(BM$ensembl_gene_id),GM_df,weighted=TRUE,func=normalize)
+	dL[[paste('geneMania_',t_i,'_net_unweightedShortestPath',sep='')]] <- Network_ShortestPaths_distance(unique(BM$ensembl_gene_id),GM_df,weighted=FALSE)	
+}
+
+
+
+
+### combine distance matrixes
+dL[['final']] <- combine(dL,sum,BM,gene_ids,gene_type)
+for(i in 1:length(dL)){
+	d_i = dL[[i]]
+	d_i_id <- names(which.max(apply(BM,2,function(x) sum(rownames(d_i) %in% x))))
+	if(!all(rownames(d_i)==colnames(d_i))){stop('distance matrix is not symetrical')}
+	rownames(d_i)=colnames(d_i)=convert(rownames(d_i),d_i_id,'hgnc_symbol',BM)
+	dL[[i]] = d_i
+}
+
+#rownames(d)=colnames(d)=convert(rownames(d),gene_type,'hgnc_symbol',BM)
+
+### cluster
+par(mfrow=c(1,3))
+plot(hclust(as.dist(dL[[1]])),main='jaccard GO cluster')
+plot(hclust(as.dist(dL[[2]])),main='jaccard GO+Ramigo cluster')
+plot(hclust(as.dist(dL[[3]])),main='shortest paths GO+Ramigo cluster')
+
+plot(hclust(as.dist(dL[[4]])),main='tissue expression correlation cluster')
+
+plot(hclust(as.dist(d)),main='combined cluster')
+
+
+
